2e7fdd96e8a2149bc849f180dba5e13136b0e40b
vladimirogaston <vladimirogaston@gmail.com>
2021-07-24 18:08:02 -0300

    add test not implemented test cases

---
+++ src/test/java/ar/ungs/infraestructure/api/resources/InspectionResourceTest.java
@@ -0,0 +1,12 @@
+package ar.ungs.infraestructure.api.resources;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InspectionResourceTest {
+
+    @Test
+    void save() {
+    }
+}
