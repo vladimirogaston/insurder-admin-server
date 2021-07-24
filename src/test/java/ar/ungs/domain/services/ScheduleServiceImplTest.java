7656238799dba09b8e68a0da7f26bddd2be8998f
vladimirogaston <vladimirogaston@gmail.com>
2021-07-24 18:14:59 -0300

    add insuranceCarrier.java

---
+++ src/test/java/ar/ungs/domain/services/ScheduleServiceImplTest.java
@@ -0,0 +1,28 @@
+package ar.ungs.domain.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScheduleServiceImplTest {
+
+    @Test
+    void readNotNotifiedByInspector() {
+    }
+
+    @Test
+    void cancel() {
+    }
+
+    @Test
+    void close() {
+    }
+
+    @Test
+    void registerComponent() {
+    }
+
+    @Test
+    void notifySchedule() {
+    }
+}
